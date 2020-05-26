f4c9d9013801824099105d405a838142ed6f7d8e
MyFriendBobSacamano <47094499+MyFriendBobSacamano@users.noreply.github.com>
2020-05-26 19:18:39 -0400

    add js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,3 @@
-// const navSlide = () => {
-//   const burger = document.querySelector('.burger');
-//   const nav = document.querySelector('.nav-links');
-//
-//   burger.addEventListener('click', ()=>){
-//     nav.classList.toggle('nav-active');
-//   });
-// }
-//
-// navSlide();
 function myFunction() {
   var x = document.getElementById("nav");
   if (x.className === "topnav") {
