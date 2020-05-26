d7e347c75df571ed9db881621acd97c7938a17c3
MyFriendBobSacamano <47094499+MyFriendBobSacamano@users.noreply.github.com>
2020-05-26 18:56:28 -0400

    javascript

--- js/app.js
+++ js/app.js
@@ -1,10 +1,18 @@
-const navSlide = () => {
-  const burger = document.querySelector('.burger');
-  const nav = document.querySelector('.nav-links');
-
-  burger.addEventListener('click', ()=>){
-    nav.classList.toggle('nav-active');
-  });
+// const navSlide = () => {
+//   const burger = document.querySelector('.burger');
+//   const nav = document.querySelector('.nav-links');
+//
+//   burger.addEventListener('click', ()=>){
+//     nav.classList.toggle('nav-active');
+//   });
+// }
+//
+// navSlide();
+function myFunction() {
+  var x = document.getElementById("nav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
-
-navSlide();
