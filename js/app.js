4d6d2a8ce2fa0ff72349ee32cd6301c3193f061a
MyFriendBobSacamano <47094499+MyFriendBobSacamano@users.noreply.github.com>
2020-05-30 00:43:36 -0400

    updating js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
-function myFunction() {
-  var x = document.getElementById("burger");
-  if (x.className === "burger") {
-    x.className += " responsive";
-  } else {
-    x.className = "burger";
-  }
-}
+// function myFunction() {
+//   var x = document.getElementById("burger");
+//   if (x.className === "burger") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = "burger";
+//   }
+// }
+let d = new Date();
+alert("Today's date is " + d);
