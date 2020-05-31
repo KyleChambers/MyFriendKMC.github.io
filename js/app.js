3166d749a91e5731779990c1dc75909920f60fc3
MyFriendBobSacamano <47094499+MyFriendBobSacamano@users.noreply.github.com>
2020-05-31 18:01:33 -0400

    delete app.js

--- js/app.js
+++
@@ -1,8 +0,0 @@
-// function myFunction() {
-//   var x = document.getElementById("burger");
-//   if (x.className === "burger") {
-//     x.className += " responsive";
-//   } else {
-//     x.className = "burger";
-//   }
-// }
